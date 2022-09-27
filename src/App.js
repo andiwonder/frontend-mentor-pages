bad2a9cdc58c273b818687043f6b98e54c236921
andiwonder <skasliwal12@gmail.com>
2022-09-27 18:51:09 -0400

    empty html

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-         Hi CannonMan !!!
+         Hi CannonMan !!! Coming Soon {'<3'}
         </p>
+        <section>
+          <div>
+            <h2>E-commerce product page</h2>
+          </div>
+          <div>
+            <h2>Rock, Paper, Scissors game</h2>
+          </div>
+          <div>
+            <h2>Intro section with dropdown navigation</h2>
+          </div>
+        </section>
       </header>
     </div>
   );
